b19669f47d956ba8ff520085890ead90b2f13e13
Julio Cesar Reyes <jcreyes@tribalworldwide.gt>
2026-04-20 21:42:21 +0200

    Initial Commit

---
+++ NASAExplorer/NASAExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASAExplorerApp.swift
+//  NASAExplorer
+//
+//  Created by Julio César Reyes on 20/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct NASAExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
